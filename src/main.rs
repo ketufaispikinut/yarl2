fe657d4376c09b183a4c4550885a6a5ad6066163
KeTuFaisPiKiNut <85769816+ketufaispikinut@users.noreply.github.com>
2025-02-16 10:59:26 -0500

    Update main.rs to have one comment

--- src/main.rs
+++ src/main.rs
@@ -1,9 +1,6 @@
-use yarl_2::{run_game, Config}; //, Window
-
+use yarl_2::{run_game, Config};
+// this runs a demo of the library
 fn main() {
-    println!("Hello, world!"); //game//config
-    run_game((), Config::default()) //Window::new_run((),Config::default());
-                                    /* loop{
-
-                                    } */
+    println!("Hello, world!"); 
+    run_game((), Config::default())
 }
